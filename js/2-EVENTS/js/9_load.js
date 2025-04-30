2f8d86100ab9fb15eebc8c562aa9f0b8b8cbb451
Mariana Alves <m.bourdain.alves>
2025-04-30 13:30:06 +0100

    Load done

--- js/2-EVENTS/js/9_load.js
+++ js/2-EVENTS/js/9_load.js
@@ -1,9 +1,10 @@
-//NOTA: Quando selecionamos document.[x], alerts, entre outros, realmengte estamos a selecionar o nosso window / browser
+/* //window.addEventListener('load', init, false);
+document.addEventListener('DOMContentLoaded', init, false);
 
-window.addEventListener('DOMContentLoaded', init, false); //DOMCOntentLoaded -> Despoleta logo que o DOM estja pronto, é mais rápido
-//load -> Despoleta só depois de todos os recursos, como, por exemplo, imagens, estejam 100% carregados, é mais lento
+function init (){ */
+    
+let username = document.querySelector('#username');
+username.value = 'teste';
 
-function init(){
-    let username = document.querySelector('#username');
-    username.value = 'teste';
-}
+
+/* } */
